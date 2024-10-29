b18c1306cef4af41d52335256fd999d9e02a4899
lawal <sphade010@gmail.com>
2024-10-29 10:40:21 +0100

    ordered tournament by desc

--- src/routes/(app)/tournament/+page.server.ts
+++ src/routes/(app)/tournament/+page.server.ts
@@ -2,7 +2,9 @@ import { error } from '@sveltejs/kit';
 
 export const load = async ({ locals: { db, user } }) => {
 	if (!user) error(401, 'Unauthorized');
-	const tournaments = await db.query.tournamentsTable.findMany();
+	const tournaments = await db.query.tournamentsTable.findMany({
+		orderBy: (t, { desc }) => desc(t.createdAt)
+	});
 	return {
 		tournaments
 	};

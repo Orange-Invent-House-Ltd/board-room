0ac59954afc653d0854f7016c7297f6e3fc7dcd2
lawal <sphade010@gmail.com>
2024-10-30 15:02:48 +0100

    fix: tournament

--- src/routes/(app)/tournament/+page.server.ts
+++ src/routes/(app)/tournament/+page.server.ts
@@ -1,7 +1,7 @@
 import { getTournaments } from '$lib/server/actions/queries';
 
 export const load = async (event) => {
-	const tournaments = await getTournaments(event);
+	const { tournaments } = await getTournaments(event);
 	return {
 		tournaments
 	};

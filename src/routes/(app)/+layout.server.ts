a0176de8c3cf065d465be3109b6bcc7cfc1bae44
lawal <sphade010@gmail.com>
2024-11-10 02:24:26 +0100

    changes

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -3,9 +3,9 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals: { user, db } }) => {
 	console.log('🚀 ~ load ~ user:', user);
-	if (!user) {
-		redirect(308, '/login');
-	}
+	// if (!user) {
+	// 	redirect(308, '/login');
+	// }
 	const userWithStats = await getUserWithStats(db, user.id);
 	return {
 		user,
